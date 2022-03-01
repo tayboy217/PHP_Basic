56a5102892240e5b36453d977131518330afda6e
tayboy217 <tayboy217@icloud.com>
2022-03-02 03:00:19 +0900

    [Update]PHP_Basic

---
+++ sample08.php
@@ -0,0 +1,7 @@
+<?php
+// $tax = 10;
+const TAX = 10;
+// define(TAX, 10);
+
+echo "消費税は、", TAX, "%です";
+
---
+++ sample09.php
@@ -0,0 +1,5 @@
+<?php
+
+$sum = 5000 + 150;
+$sum = number_format(1234.12, 2, ':', ';');
+echo $sum, '円です';
