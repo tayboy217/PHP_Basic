4fcf51801ac0eee8cf27a711969d29b7e33a1466
tayboy217 <tayboy217@icloud.com>
2022-03-08 23:58:24 +0900

    [Update]PHP_Basic

---
+++ 3-8.php
@@ -0,0 +1,12 @@
+<?php
+$pref = [
+  'hokkaidou' => '北海道',
+  'aomori' => '青森県',
+  'iwate' => '岩手県',
+  'akita' => '秋田県'
+];
+
+foreach ($pref as $pref_key => $pref_name) {
+  echo '・',$pref_key,':', $pref_name,"\n";
+}
+
